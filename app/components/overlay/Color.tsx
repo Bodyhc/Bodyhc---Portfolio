88674c7cb18ecbcc7183905b800a1b27d8fd43c5
Abdullah M Soliman <94320243+Bodyhc@users.noreply.github.com>
2026-02-09 01:17:52 +0200

    Delete app directory

--- app/components/overlay/Color.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Color: React.FC = () => {
-    return (
-        <div className="fixed inset-0 p-1 w-full h-full bg-gradient-to-t from-bg-dark to-transparent opacity-20 pointer-events-none z-20"/>
-    );
-};
-
-export default Color;
