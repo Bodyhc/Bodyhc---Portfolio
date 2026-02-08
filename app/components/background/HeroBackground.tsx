137e9c8a2b83e3f96d89dcfd648554f6bf4926e9
Abdullah M Soliman <94320243+Bodyhc@users.noreply.github.com>
2026-02-08 14:07:43 +0200

    Add files via upload

--- app/components/background/HeroBackground.tsx
+++ app/components/background/HeroBackground.tsx
@@ -30,8 +30,8 @@ const HeroBackground: React.FC = () => {
             opacity: number;
 
             constructor() {
-                this.x = Math.random() * canvas.width;
-                this.y = Math.random() * canvas.height;
+                this.x = Math.random() * canvas!.width;
+                this.y = Math.random() * canvas!.height;
                 this.size = Math.random() * 3 + 1;
                 this.speedX = (Math.random() - 0.5) * 0.5;
                 this.speedY = (Math.random() - 0.5) * 0.5;
@@ -50,10 +50,10 @@ const HeroBackground: React.FC = () => {
                 this.x += this.speedX;
                 this.y += this.speedY;
 
-                if (this.x > canvas.width) this.x = 0;
-                if (this.x < 0) this.x = canvas.width;
-                if (this.y > canvas.height) this.y = 0;
-                if (this.y < 0) this.y = canvas.height;
+                if (this.x > canvas!.width) this.x = 0;
+                if (this.x < 0) this.x = canvas!.width;
+                if (this.y > canvas!.height) this.y = 0;
+                if (this.y < 0) this.y = canvas!.height;
             }
 
             draw() {
